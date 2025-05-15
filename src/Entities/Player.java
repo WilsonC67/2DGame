5613a5c0cecf75f898acfd8e588200d96ca0e3a9
WilsonC67 <wilson.chen@quinnipiac.edu>
2025-05-15 13:46:08 -0400

    Implemented the other 4 sprites to show character movement.

--- src/Entities/Player.java
+++ src/Entities/Player.java
@@ -16,6 +16,7 @@ public class Player extends Entity {
     GamePanel gamePanel;
     KeyboardInputHandler keyboard;
     BufferedImage image;
+    
 
     public Player(GamePanel gamePanel, KeyboardInputHandler keyboard) {
         // using "this" keyword because the parameters for this constructor and names of the variables are the same
@@ -24,7 +25,6 @@ public class Player extends Entity {
 
 
         setDefaultValues();
-        // getPlayerImage(); // keyboard isn't receiving feedback?
     }
 
     public void setDefaultValues() {
@@ -34,32 +34,76 @@ public class Player extends Entity {
         direction = "left";
     }
 
-    public void getPlayerImage() {
+    public void update() {
+        // purpose of this if block is to make it so that the spriteCounter only increments
+        // only if the user is pressing a movement key
+        if (keyboard.upPressed == true || keyboard.downPressed == true || 
+            keyboard.leftPressed == true || keyboard.rightPressed == true) {
+
+            if (keyboard.upPressed == true) {
+                direction = "up";
+                y -= speed;
+
+            } else if (keyboard.downPressed == true) {
+                direction = "down";
+                y += speed;
+
+            } else if (keyboard.leftPressed == true) {
+                direction = "left";
+                x -= speed;
+
+            } else if (keyboard.rightPressed == true) {
+                direction = "right";
+                x += speed;
+            }
+
+            // this update method gets called 60 times per second
+            // every frame, spriteCounter is incremented; when this reaches 10, it changes the sprite
+            // this means that the playerSprite changes every 10 frames
+            spriteCounter++;
+            if (spriteCounter > 10) {
+                if (spriteNum == 1) {
+                    spriteNum = 2;
+                } else if (spriteNum == 2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+            // System.out.println(String.format("X: %d\nY:%d", playerX, playerY));
+        }
+
+    }
+
+    public void draw(Graphics2D g2) {
+
+        // g2.setColor(Color.WHITE);
+        // // this will draw a rectangle with the color that was declared last; in this case: white
+        // g2.fillRect(x, y, gamePanel.tileSize, gamePanel.tileSize);
+
+        File f1 = new File("./src/res/player/slime_up_1.png");
+        File f2 = new File("./src/res/player/slime_up_2.png");
+        File f3 = new File("./src/res/player/slime_down_1.png");
+        File f4 = new File("./src/res/player/slime_down_2.png");
+        File f5 = new File("./src/res/player/slime_left_1.png");
+        File f6 = new File("./src/res/player/slime_left_2.png");
+        File f7 = new File("./src/res/player/slime_right_1.png");
+        File f8 = new File("./src/res/player/slime_right_2.png");
 
         try {
-
-            File f1 = new File("./src/res/player/slime_up_1.png");
             up1 = ImageIO.read(f1);
 
-            File f2 = new File("./src/res/player/slime_up_2.png");
             up2 = ImageIO.read(f2);
 
-            File f3 = new File("./src/res/player/slime_down_1.png");
             down1 = ImageIO.read(f3);
 
-            File f4 = new File("./src/res/player/slime_down_2.png");
             down2 = ImageIO.read(f4);
 
-            File f5 = new File("./src/res/player/slime_left_1.png");
             left1 = ImageIO.read(f5);
 
-            File f6 = new File("./src/res/player/slime_left_2.png");
             left2 = ImageIO.read(f6);
 
-            File f7 = new File("./src/res/player/slime_right_1.png");
             right1 = ImageIO.read(f7);
 
-            File f8 = new File("./src/res/player/slime_right_2.png");
             right2 = ImageIO.read(f8);
 
         }
@@ -67,72 +111,42 @@ public class Player extends Entity {
         catch(IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public void update() {
-
-        System.out.println(String.format("%d %d %d", x, y, speed));
-
-        System.out.println(keyboard.upPressed);
-        System.out.println(keyboard.leftPressed);
-        System.out.println(keyboard.rightPressed);
-        System.out.println(keyboard.downPressed);
-
-
-        if (keyboard.upPressed == true) {
-            direction = "up";
-            y -= speed;
-
-        } else if (keyboard.downPressed == true) {
-            direction = "down";
-            y += speed;
-
-        } else if (keyboard.leftPressed == true) {
-            direction = "left";
-            x -= speed;
-
-        } else if (keyboard.rightPressed == true) {
-            direction = "right";
-            x += speed;
-        }
-
-        // System.out.println(String.format("X: %d\nY:%d", playerX, playerY));
-    }
-
-    public void draw(Graphics2D g2) {
-
-        // File f1 = new File("./src/res/player/slime_up_1.png");
-        // try {
-        //     up1 = ImageIO.read(f1);
-        // } catch (IOException e) {
-        //     // TODO Auto-generated catch block
-        //     e.printStackTrace();
-        // }
-
-        // image = up1;
-
-        g2.drawImage(image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
-
-        // g2.setColor(Color.WHITE);
-        // // this will draw a rectangle with the color that was declared last; in this case: white
-        // g2.fillRect(x, y, gamePanel.tileSize, gamePanel.tileSize);
-
-        image = up1;
+        
+        image = null;
 
         switch(direction) {
             case "up":
-                image = up1;
+                if (spriteNum == 1) {
+                    image = up1;
+                } else if (spriteNum == 2) {
+                    image = up2;
+                }
                 break;
+
             case "down":
-                image = down1;
+                if (spriteNum == 1) {
+                    image = down1;
+                } else if (spriteNum == 2) {
+                    image = down2;
+                }
                 break;
+
             case "left":
-                image = left1;
+            if (spriteNum == 1) {
+                    image = left1;
+                } else if (spriteNum == 2) {
+                    image = left2;
+                }
                 break;
+                
             case "right":
-                image = right1;
+            if (spriteNum == 1) {
+                    image = right1;
+                } else if (spriteNum == 2) {
+                    image = right2;
+                }
                 break;
         }
-        // g2.drawImage(image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
+        g2.drawImage(image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
     }
 }
