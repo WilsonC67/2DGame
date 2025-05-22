917ed5d734115c96e5c4eca6534e4bedc1cc051f
WilsonC67 <wilson.c6921@gmail.com>
2025-05-21 23:50:04 -0400

    Added a map.

--- src/Entities/Entity.java
+++ src/Entities/Entity.java
@@ -3,12 +3,19 @@ import java.awt.image.BufferedImage;
 
 // parent class for any player classes, characters, etc
 public class Entity {
+    // x and y coordinates for the entities
     public int x, y;
+
+    // how many pixels the entity travels in one frame
     public int speed;
 
+    // 8 images to represent the entity; 2 for each cardinal direction
     public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+
+    // String that contains the direction of which the entity is traveling
     public String direction;
     
+    // these two int values are for fluctuating between the two images to imitate a more realistic walking movement
     public int spriteCounter = 0;
     public int spriteNum = 1;
 }
